15e2d2a5478b35c69e20243906d17eeba690a36a
sanjining <2506294023@qq.com>
2022-08-06 18:06:47 +0800

    gengx

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import button from './components/button.vue'
+import button from './components/button/index.vue'
 
 export default {
   name: 'App',
---
+++ src/components/button/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button
+
+    >
+        按钮
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:Sj-button
+    
+}
+</script>
+
+<style>
+
+</style>
